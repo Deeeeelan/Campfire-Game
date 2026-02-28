846f299d51450793232b7f473db2bb2b900c207e
Dylan Tseng <108163886+Deeeeelan@users.noreply.github.com>
2026-02-28 13:09:13 -0800

    Add camera zoom

--- Scripts/character_body_2d.gd
+++ Scripts/character_body_2d.gd
@@ -5,11 +5,22 @@ extends CharacterBody2D
 @export var jump_velocity = -180.0
 @export var lerp_speed = 10.0
 @export var depth : int = 0
+@export var zoom : float = 4.0
 
 var direction = 0.0
+func _input(event) -> void:
+	if event is InputEventMouseButton:
+		if event.is_pressed():
+			print(zoom)
+			if event.button_index == MOUSE_BUTTON_WHEEL_UP and zoom < 8.0:
+				zoom += 0.5
+			if event.button_index == MOUSE_BUTTON_WHEEL_DOWN and zoom > 1.0:
+				zoom -= 0.5
 
 func _physics_process(delta: float) -> void:
-	depth = self.transform.origin.y
+	depth = self.transform.origin.y 
+	$Camera2D.zoom = Vector2.ONE * zoom # TODO: Tween camera position 
+	
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
