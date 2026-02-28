e315e206bb40a72f21fc5586736faf089b1b6d14
Dylan Tseng <108163886+Deeeeelan@users.noreply.github.com>
2026-02-28 12:48:02 -0800

    Add dirt grid generation

--- Scripts/game_script.gd
+++ Scripts/game_script.gd
@@ -1,11 +1,19 @@
 extends Node
 
+@onready var tile_map = $Node2D/TileMapLayer
 
-# Called when the node enters the scene tree for the first time.
+
+func generate_terrain(origin : Vector2i, start_depth : int, width : int, depth: int):
+	print("Generating", origin, width)
+	 # Placeholder, this needs to be streamed/generated as the player moves later
+	for y in range(origin.y, depth):
+		for x in range(origin.x, width):
+			tile_map.set_cell(Vector2i(x, y), 0, Vector2i(0, 0))
+			
+	
+
 func _ready() -> void:
-	pass # Replace with function body.
+	generate_terrain(Vector2i(0, 2), 2, 100, 100)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
