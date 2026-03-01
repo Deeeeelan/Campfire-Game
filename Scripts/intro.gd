463f233bed91ff2734dd2b9baf80eeca6f1b8ad4
Lakshya Raj <63673413+lraj22@users.noreply.github.com>
2026-03-01 13:57:15 -0800

    Fix grammatical mistake in intro messages

--- Scripts/intro.gd
+++ Scripts/intro.gd
@@ -5,7 +5,7 @@ var is_ready = false
 var info_messages = [
 	"Calamity. Ever since the year 2032, that's all the people of the city of Minville knew.",
 	"A brave miner named Joe knew he had to escape. There was nothing left to have done on the surface, so he started to dig. No one left to judge him, anyways.",
-	"You must help him secure himself, away from all the mobs and storms above, chasing him to the depths of the world. Can you help save Joe from the dangers of the world by digging him down to the safe bunker below?",
+	"You must help him secure himself, away from all the mobs and storms above chasing him to the depths of the world. Can you help save Joe from the dangers of the world by digging him down to the safe bunker below?",
 ]
 
 var index = 0
