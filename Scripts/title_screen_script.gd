3a1f0cb70c5aee9231887cafba4e831a2039dc9b
Dylan Tseng <108163886+Deeeeelan@users.noreply.github.com>
2026-02-28 17:19:53 -0800

    Fix quit button

--- Scripts/title_screen_script.gd
+++ Scripts/title_screen_script.gd
@@ -3,9 +3,12 @@ extends Node
 var game_path = "res://Scenes/Game.tscn"
 
 @onready var start_button = $StartButton
-
+@onready var quit_button = $QuitButton
 func start():
 	get_tree().change_scene_to_file(game_path)
+func quit():
+	get_tree().quit()
 
 func _ready() -> void:
 	start_button.pressed.connect(start)
+	quit_button.pressed.connect(quit)
