eb3943ef4e63ba9c4375d3b2700f7ac5975e9d8f
Lavith Raj <189830163+lraj23@users.noreply.github.com>
2026-03-01 16:04:26 -0800

    made mobs attempt to dig

--- Scripts/mob.gd
+++ Scripts/mob.gd
@@ -8,33 +8,39 @@ extends CharacterBody2D
 @export var lerp_speed = 5.5 # lower = more slippery
 @export var zoom : float = 8.0
 @export var game_ticker: Timer
+@export var game_ticker2: Timer
+
 @export var tile_map: TileMapLayer
 var direction = 0.0
 
 var initialization_time
 
-# Called when the node enters the scene tree for the first time.
+func die():
+	queue_free()
+
+# Called when the node e	nters the scene tree for the first time.
 func _ready() -> void:
 	print("Mob initialized")
-	game_ticker.timeout.connect(tick)
-	initialization_time=Time.get_unix_time_from_system()
+	self.position.x=get_node("/root/Game/Node2D/CharacterBody2D").position.x
+	game_ticker.timeout.connect(die)
+	game_ticker2.timeout.connect(tick)
+
 	pass
 
 func dig(pos: Vector2i):
-	pass
+	tile_map.set_cell(pos, 0, Vector2i(0, 2))
 
 func tick():
 	var player=get_node("/root/Game/Node2D/CharacterBody2D")
-	print("Tick", self.position, player.position)
-	if(self.position.y-player.position.y<16):
+	print("Tick", position, player.position)
+	if position.y-player.position.y<-16:
 		velocity += get_gravity() * 1
-		var self_pos = tile_map.local_to_map(self.position)
-		print(self_pos)
+		var self_pos = tile_map.local_to_map(position)
+		print("Self Position", self_pos, self_pos.x, self_pos.y)
+		self_pos.y -= 1;
 		dig(self_pos)
 	else:
 		velocity.x = direction * speed
-	if(Time.get_unix_time_from_system()-initialization_time>3):
-		queue_free()
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
